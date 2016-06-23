e8ba8559b3a6b02e2ad4026772351cac203414cc
RahulBaboota <rahulbaboota08@gmail.com>
2016-06-23 19:53:40 +0530

    Completed Exercise 2

--- Week 1/Homework 1/Homework1.r
+++ Week 1/Homework 1/Homework1.r
@@ -9,9 +9,15 @@ climate_change = read.csv('climate_change')
 climate_change_training = subset( climate_change , Year <= 2006)
 
 #The rest of the data is classified as testing data
-climate_change_testing  = subset( climate_change , Year >2006 )
+climate_change_testing  = subset( climate_change , Year > 2006 )
 
 #Firstly , we create a linear regression model considering all independent variables excluding Year and Month 
 TempRegModel1 = lm(Temp ~ MEI + CO2 + CH4 + N20 + CFC.11 + CFC.12 + TSI + Aerosols , data = climate_change_training)
 summary(TempRegModel1)
+
+#Now to check the correlation among different independent variables , we can use the cor function.
+#The study of correlation will help us to explain some of the anamolous behaviours in our predictions
+cor(climate_change_training)
+
+
 

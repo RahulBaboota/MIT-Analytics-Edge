173612645efc1a998877c362fd0f2030dcc85a02
RahulBaboota <rahulbaboota08@gmail.com>
2016-07-10 10:21:14 +0530

    Solved Problem 2

---
+++ Week2/Homework2/Homework2.r
@@ -0,0 +1,30 @@
+## Solutions for Homework 2
+
+###									Setting up the file and variable names
+
+# Firstly , we will read in the paroles csv file
+parole = read.csv('parole.csv')
+
+## -----------------------------------------------------------------------------------------------------------------------------
+
+## Problem 1
+
+## Initially , we just want to find out the number of observations in the dataset . For this , we can use either of the two methods .
+str(parole)
+nrow(parole)
+
+## Next , we find out the number of parolees who violated their parole . For this , we use the table function .
+table(parole$violator)
+
+## -----------------------------------------------------------------------------------------------------------------------------
+
+## Problem 2
+
+## The variables male, race, state, crime, and violator all are factor variables and all of them are unordered . However , out of 
+## these , only state and crime have at least 3 levels .
+
+## Since these variables are factor variables with 3 levels, we convert them into factor variables . 
+parole$state = as.factor(parole$state)
+parole$crime = as.factor(parole$crime)
+
+## -----------------------------------------------------------------------------------------------------------------------------

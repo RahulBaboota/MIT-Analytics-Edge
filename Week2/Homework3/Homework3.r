15e0b31c032f960922cec68df71de2f1d7001593
RahulBaboota <rahulbaboota08@gmail.com>
2016-07-27 16:39:49 +0530

    Solved Problem 3

--- Week2/Homework3/Homework3.r
+++ Week2/Homework3/Homework3.r
@@ -54,6 +54,7 @@ spl = sample.split( loans , SplitRatio = 0.7 )
 Train = subset( loans , spl == TRUE )
 Test = subset( loans , spl == FALSE )
 Model1 = glm( not.fully.paid ~ . , data = Train , family = binomial ) 
+summary(Model1)
 
 ## Next , we test our predictions on the testing set and compare the accuracy of our created model with the baseline model .
 
@@ -74,6 +75,40 @@ as.numeric(performance(pred, "auc")@y.values)
 
 ## -----------------------------------------------------------------------------------------------------------------------------
 
+## Problem 3
+
+## In our dataset , we have an independent variable interest rate which is given as "int.rate" . This value is decided depending on 
+## the probability of wether the loan will be paid back or not . If the risk of not paying back the loan is high , the interest 
+## rate is also increased and vice - versa .
+
+## Let us now create a bivariate logistic regression model ( a model with only 1 independent variable ) in which our independent
+## variable will be "int.rate" . 
+
+Model2 = glm( not.fully.paid ~ int.rate , family = binomial , data = Train )
+summary(Model2)
+
+## By looking at the summary of the model created above , we can see that now int.rate is a significant variable whereas in the model
+## that we created initially in which we used all of the variables ,  int.rate was not significant . This mostly happens due to
+## a strong correlation between the independent variables and on further investigating , we can see that int.rate and fico are
+## somewhat highly correlated .
+
+cor( Train$int.rate , Train$fico )
+
+## Next , we try to create a smarter baseline model using the model that we just created above . 
+
+PredTest2 = predict( Model2 , newdata = Test , type = "response" )
+summary(PredTest2)
+
+## Looking at the summary of the model , we can clearly see that the maximum probability that the model returns on the testing set
+## is less than 0.5 , which means that if we take the threshold to be 0.5 , the model would predict that all of the loans would
+## be paid .
+
+## Next , let us calculate the AUC of this model .
+
+Pred2 = prediction( PredTets2 , Test$not.fully.paid )
+as.numeric(performance(pred , "auc")@y.values)
+
+## -----------------------------------------------------------------------------------------------------------------------------
 
 
 

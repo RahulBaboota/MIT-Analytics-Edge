3673fd2054e6e997cb95876aa87cc6c32f8efe57
RahulBaboota <rahulbaboota08@gmail.com>
2016-08-02 23:44:35 +0530

    Solved Problem 3

--- Week3/Homework1/Homework1.r
+++ Week3/Homework1/Homework1.r
@@ -31,7 +31,42 @@ prp(CARTModel2)
 
 ## We now create another Regression Tree , this time , including the "sex" variable in our observations.
 
-CARTModel3 = rpart( voting ~ civicduty + hawthorne + self + neighbors + sex, data = Gerber )
+CARTModel3 = rpart( voting ~ civicduty + hawthorne + self + neighbors + sex, data = Gerber , cp =0.0 )
 prp(CARTModel3)
+
+## -----------------------------------------------------------------------------------------------------------------------------
+
+## Problem 3
+
+## Now , let us create another Regression Model using just the "control" variable and "control" and "sex" variable .
+
+CARTModel4 = rpart( voting ~ control , data = Gerber , cp = 0.0 )
+prp(CARTModel4)
+
+CARTModel5 = rpart( voting ~ control + sex , data = Gerber , cp = 0.0 )
+prp(CARTModel5)
+
+## Now , let us a create a logistic regression model using these independent variables only .
+
+LogisticModel1 = glm( voting ~ control + sex , data = Gerber , family = "binomial" )
+summary(LogisticModel1)
+
+## We saw that our Regression Tree was able to provide us with the probablities of all cases which were (Man, Not Control),
+## (Man, Control), (Woman, Not Control), (Woman, Control) . However , our logistic regression model is not able to do that .
+## Therefore , we create a new dataframe which contains all the possible values of "control" and "sex" and apply the predict 
+## function on this dataframe .
+
+Possibilities = data.frame(sex=c(0,0,1,1),control=c(0,1,0,1))
+predict(LogisticModel1, newdata=Possibilities, type="response")
+
+## We now add another variable in our logistic regression model that is the combination of the "sex" and "control" variables - 
+## so if this new variable is 1, that means the person is a woman AND in the control group.
+
+LogisticModel2 = glm(voting ~ sex + control + sex:control, data=gerber, family="binomial")
+summary(LogisticModel2)
+
+Possibilities = data.frame(sex=c(0,0,1,1),control=c(0,1,0,1))
+predict(LogisticModel2, newdata=Possibilities, type="response")
+
 
 ## -----------------------------------------------------------------------------------------------------------------------------

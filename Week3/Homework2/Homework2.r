e8d4124db153c387fd3781bbed99c9c17231b979
RahulBaboota <rahulbaboota08@gmail.com>
2016-08-04 20:12:45 +0530

    Solved Problem 2

--- Week3/Homework2/Homework2.r
+++ Week3/Homework2/Homework2.r
@@ -47,3 +47,48 @@ Predictions2 = predict( CartModel2 , newdata = Test )
 table( Test$isB , Predictions2 )
 
 ## -----------------------------------------------------------------------------------------------------------------------------
+
+## Problem 2
+
+## Now , we will solve the original problem of predicting the letter . 
+
+## For this , we firstly need to convert the "letter" variable into a factor variable .
+
+Letters$letter = as.factor(Letters$letter)
+
+## Then , we need to create new Training and Testing Data .
+
+library(caTools)
+set.seed(2000)
+spl = sample.split( Letter$letters , SplitRatio = 0.5 )
+Train = subset( Letters , spl == TRUE )
+Test = subset( Letters , spl == FALSE )
+
+## After that we will create a baseline model which is nothing but assigning the most frequently occuring value in the Training data
+## to all the observations in the Test data .
+
+table( Train$letters )
+
+## Let us now create a CART Model to predict the letter .
+
+library(rpart)
+library(rpart.plot)
+
+CartModel3 = rpart( letters ~ . - isB , data = Train , method = "class" )
+Predictions3 = predict( CartModel3 , newdata = Test , type = "class" )
+
+## Now , let us compute the accuracy of our model by creating a confusion matrix .
+table( Test$letter , Predictions3 )
+
+## Now , let us create a Random Forest Model for the same .
+
+library(randomForest)
+set.seed(1000)
+
+CartModel4 = randomForest( letters ~ . - isB , data = Train )
+Predictions2 = predict( CartModel4 , newdata = Test )
+
+## Now , let us check the accuracy of our model by creating a confusion matrix .
+table( Test$isB , Predictions4 )
+
+## -----------------------------------------------------------------------------------------------------------------------------
